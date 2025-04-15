d85560fd3649f6ce43dc464061718dd09b293925
To Nguyen <tonguyen0903@gmail.com>
2025-04-15 22:36:54 +0700

    fix build1

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+// main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HashRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </HashRouter>
-  </StrictMode>
+    </BrowserRouter>
+  </React.StrictMode>
 )
